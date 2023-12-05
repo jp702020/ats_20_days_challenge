381e2fb3bc158e163bd5945e5e0d4bb6b87864ed
Patel Junaid <jp70202021@gmail.com>
2023-12-05 14:45:41 +0530

    first coment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nanbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Nanbar />
+        <Routes>
+          <Route exact path="/" Component={Home}></Route>
+          <Route exact path="/about" Component={About}></Route>
+          <Route exact path="/contact" Component={Contact}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Terms of Service</p>
+      <p>Privacy Policy</p>
+    </footer>
+  );
+};
+
+export default Footer;
